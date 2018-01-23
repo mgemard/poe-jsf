f1b5d8aa6c40898990e1c197ef0bc7160dc2594f
mgemard <mathieu.gemard@gmail.com>
2018-01-23 16:51:13 +0100

    added webservice get and post with json

---
+++ poe-jsf/src/poe/rest/UserRest.java
@@ -0,0 +1,67 @@
+package poe.rest;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
+import poe.dao.UserDao;
+import poe.jpa.User;
+
+@Path("user")
+public class UserRest {
+
+    @EJB
+    private UserDao userDao;
+
+    @GET
+    @Produces("application/json")
+    @Path("/show")
+    public User show(@QueryParam("id") int id) {
+        return userDao.get(id);
+        // User user = new User();
+        // user.setEmail("mail@mail.com");
+        // return user;
+    }
+
+    @DELETE
+    @Path("/delete")
+    public void delete(@QueryParam("id") int id) {
+        userDao.delete(id);
+        // User user = new User();
+        // user.setEmail("mail@mail.com");
+        // return user;
+    }
+
+    @POST
+    @Path("/")
+    public void post(@QueryParam("email") String email, @QueryParam("password") String password) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        userDao.add(user);
+        // User user = new User();
+        // user.setEmail("mail@mail.com");
+        // return user;
+    }
+
+    @POST
+    @Consumes("application/json")
+    @Path("/addJson")
+    public void post(User user) {
+        userDao.update(user);
+        // User user = new User();
+        // user.setEmail("mail@mail.com");
+        // return user;
+    }
+    
+    
+    
+    
+    
+
+}
