fbfa9129f5855cf10b4abcadcc26b5555d67dd8d
mgemard <mathieu.gemard@gmail.com>
2018-01-24 09:49:25 +0100

    changed rest naming paths

--- poe-jsf/src/poe/rest/UserRest.java
+++ poe-jsf/src/poe/rest/UserRest.java
@@ -1,18 +1,21 @@
 package poe.rest;
 
+import java.util.List;
+
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import poe.dao.UserDao;
 import poe.jpa.User;
 
-@Path("user")
+@Path("users")
 public class UserRest {
 
     @EJB
@@ -20,16 +23,20 @@ public class UserRest {
 
     @GET
     @Produces("application/json")
-    @Path("/show")
-    public User show(@QueryParam("id") int id) {
+    @Path("{id}")
+    public User show(@PathParam("id") int id) {
         return userDao.get(id);
-        // User user = new User();
-        // user.setEmail("mail@mail.com");
-        // return user;
+    }
+
+    @GET
+    @Produces("application/json")
+    @Path("/")
+    public List<User> show() {
+        return userDao.getUsers();
     }
 
     @DELETE
-    @Path("/delete")
+    @Path("delete")
     public void delete(@QueryParam("id") int id) {
         userDao.delete(id);
         // User user = new User();
@@ -38,7 +45,7 @@ public class UserRest {
     }
 
     @POST
-    @Path("/")
+    @Path("add")
     public void post(@QueryParam("email") String email, @QueryParam("password") String password) {
         User user = new User();
         user.setEmail(email);
@@ -90,7 +97,7 @@ content-type: application/json
     "email": "mailazfa",
     "password": "secretpasswordfafa"
 }*/
-     */
+     
     
 
 }
